09d776ef48763a8a75e996c994adf022f2244adb
yns-qara <younesqara@gmail.com>
2022-10-16 19:22:31 +0100

    💸 basic html structure

---
+++ components/Navbar.js
@@ -0,0 +1,21 @@
+import styles from "../styles/navbar.module.css"
+const Navbar = () => {
+  return (
+    <nav>
+        <div>Logo</div>
+        <div>
+            <ul>
+                <li>News&Featured</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>Kids</li>
+                <li>Sale</li>
+                <li>Gifts</li>
+            </ul>
+        </div>
+        <div>cart</div>
+    </nav>
+  )
+}
+
+export default Navbar

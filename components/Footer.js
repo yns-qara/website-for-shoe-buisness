eff65c88aa815cf02d7acb4313778bc36f9c4fe6
yns-qara <younesqara@gmail.com>
2022-10-17 22:33:14 +0100

    👌 basic footer markup

---
+++ components/Footer.js
@@ -0,0 +1,35 @@
+
+const Footer = () => {
+  return (
+    <div>
+        <div>
+            {/* left footer */}
+            <div>Logo</div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum, impedit? Doloribus illum ipsum, laborum ad eum eos maiores nam rem molestias voluptate officiis? Earum molestias placeat explicabo. Non, alias consequatur, delectus optio molestiae unde saepe in culpa aliquid dolor iste tempore ab numquam aspernatur dolores quisquam minima et totam ea sit aut quidem ut. Eligendi incidunt velit maiores sunt voluptatum, amet, deserunt autem perspiciatis quia possimus assumenda nihil. Officia eaque tenetur ipsa unde repellat maiores iste dolor doloribus veritatis, quis maxime nesciunt vero ullam, eveniet quidem sint deleniti similique impedit?</p>
+            <div>
+                <button>visit 1</button>
+                <button>visit 2</button>
+            </div>
+
+        </div>
+
+        <div>
+            {/* right foooter */}
+            <ul>
+                <li><h1>title 1</h1></li>
+                <li>item 1</li>
+                <li>item 2</li>
+                <li>item 3</li>
+            </ul>
+            <ul>
+                <li><h1>title 2</h1></li>
+                <li>item 1</li>
+                <li>item 2</li>
+                <li>item 3</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Footer

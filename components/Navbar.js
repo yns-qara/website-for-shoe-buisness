2013bba8b37f499b9671f5860665b55d114de5f5
yns-qara <younesqara@gmail.com>
2022-10-19 18:27:08 +0100

    more nav styling 😍

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,28 @@
 import styles from "../styles/navbar.module.css"
 import Link from "next/link"
+import Cart from "../components/svgs/Cart"
+import Logo from "../components/svgs/Logo"
 const Navbar = () => {
   return (
     <nav className={styles.container}>
-        <div>Logo</div>
+        <div className={styles.logo}>
+          {/* <Logo /> */}
+          Logo
+        </div>
         <div className={styles.list}>
             <ul>
-                <li><Link href="#">News&Featured</Link></li>
+                <li><Link href="#">Featured</Link></li>
                 <li><Link href="#">Men</Link></li>
                 <li><Link href="#">Women</Link></li>
                 <li><Link href="#">Kids</Link></li>
                 <li><Link href="#">Sale</Link></li>
+                <li><Link href="#">collections</Link></li>
                 <li><Link href="#">Gifts</Link></li>
             </ul>
         </div>
-        <div>cart</div>
+        <div>
+            <Cart />
+        </div>
     </nav>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>  
       <Navbar />
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
